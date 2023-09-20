1a6b057ca36024c8ae450d7d4f006fa1863f9893
Willy <willykipchumba22@gmail.com>
2023-09-20 17:04:52 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(a = "roller-skate") {
+    let ans = `This Saturday, I want to ${a}!`;
+    return ans;
+  }
+  
+  function mondayWork(a = "go to the office") {
+    let ans = `This Monday, I will ${a}.`;
+    return ans;
+  }
+  
+  function wrapAdjective(a = "*") {
+    const inner = function (b = "special") {
+      return `You are ${a}${b}${a}!`;
+    };
+    return inner;
+  }
